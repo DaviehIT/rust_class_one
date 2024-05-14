a2440ebcff0be86b5147418b49125ae5f4bf711d
DaviehIT <davieh_it@DJRICKN>
2024-05-14 15:33:40 +0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    
+
+ //array
+
+ let array=[5,10,15,20,50];
+ let mut index=0;
+ while index <6 {
+    if array [index] <10 {
+        println!("is less than 10");
+
+    }
+    if array [index] >20 {
+        println!("greater than 10");
+    }
+    index +=1;
+ }
+
+
+}
