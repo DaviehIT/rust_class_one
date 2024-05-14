08152fbed66db2a12ea871ae15753e44f5aa789e
DaviehIT <davieh_it@DJRICKN>
2024-05-14 16:22:46 +0300

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ fn main() {
     
 
  //array
+ /* 
 
- let array=[5,10,15,20,50];
+  let array=[5,10,15,20,50];
  let mut index=0;
  while index <6 {
     if array [index] <10 {
@@ -15,6 +16,13 @@ fn main() {
     }
     index +=1;
  }
+ */
+
+
+ let number_1:i32=101;
+ let mut number_1_string:String=number_1.to_string();
+ number_1_string.push_str("people in the room");
+ println!("Hey {number_1_string}");
 
 
 }
